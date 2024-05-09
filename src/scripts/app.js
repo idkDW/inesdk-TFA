2e89f0740c039c69e1699f68830a63ba2a524986
i_dk <dalsbaekines@gmail.com>
2024-05-09 21:47:52 +0200

    avancement

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,21 +1,33 @@
 'use strict'
 
+
 document.addEventListener("DOMContentLoaded", function() {
-    const projetsEl = document.querySelectorAll('.projets__el');
+    const tousEl = document.querySelector('.projets__el-tous');
+    const projetsEl = document.querySelector('.projets__el-projets');
+    const themesEl = document.querySelector('.projets__el-themes');
     const projBtns = document.querySelector('.projets__proj-btn');
     const themeBtns = document.querySelector('.projets__theme-btn');
   
-    projetsEl.forEach(el => {
-      el.addEventListener('click', function() {
-        // On vérifie quel élément a été cliqué
-        if (el.textContent.trim() === 'PROJETS') {
-          projBtns.style.display = 'flex';
-          themeBtns.style.display = 'none';
-        } else if (el.textContent.trim() === 'THÈMES') {
-          projBtns.style.display = 'none';
-          themeBtns.style.display = 'flex';
-        }
-      });
-    });
-  });
+    // Fonction pour masquer les boutons
+    function masquerBoutons() {
+        projBtns.style.display = 'none';
+        themeBtns.style.display = 'none';
+    }
   
+    tousEl.addEventListener('click', function() {
+        masquerBoutons(); // Masquer les boutons
+    });
+  
+    projetsEl.addEventListener('click', function() {
+        projBtns.style.display = 'flex';
+        themeBtns.style.display = 'none';
+    });
+  
+    themesEl.addEventListener('click', function() {
+        projBtns.style.display = 'none';
+        themeBtns.style.display = 'flex';
+    });
+  
+    // Au chargement de la page, masquer les boutons
+    masquerBoutons();
+});
