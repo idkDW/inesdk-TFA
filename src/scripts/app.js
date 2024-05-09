4f4a6d75dfdfa6b390f57c2f2873e570cfa535b4
i_dk <dalsbaekines@gmail.com>
2024-05-09 20:36:17 +0200

    test js ça marche pass

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,21 @@
 'use strict'
 
+document.addEventListener("DOMContentLoaded", function() {
+    const projetsEl = document.querySelectorAll('.projets__el');
+    const projBtns = document.querySelector('.projets__proj-btn');
+    const themeBtns = document.querySelector('.projets__theme-btn');
+  
+    projetsEl.forEach(el => {
+      el.addEventListener('click', function() {
+        // On vérifie quel élément a été cliqué
+        if (el.textContent.trim() === 'PROJETS') {
+          projBtns.style.display = 'flex';
+          themeBtns.style.display = 'none';
+        } else if (el.textContent.trim() === 'THÈMES') {
+          projBtns.style.display = 'none';
+          themeBtns.style.display = 'flex';
+        }
+      });
+    });
+  });
+  
