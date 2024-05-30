3926be5d80801a6bfc5d57829e7cf10434e433e2
i_dk <dalsbaekines@gmail.com>
2024-05-31 00:00:59 +0200

    ahhhhh

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -55,7 +55,7 @@ if(document.body.classList.contains("index-body")){
 
 document.addEventListener("DOMContentLoaded", function() {
   var currentImageIndex = 1;
-  var totalImages = 3; // Total number of images
+  var totalImages = 4; // Total number of images
 
   // Fonction pour changer l'image séquentiellement
   function changeImage() {
@@ -403,3 +403,48 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
+
+
+  /* créer un slider*/
+
+"use strict";
+
+let prevButton = document.querySelector(".btn__prev");
+let nextButton = document.querySelector(".btn__next");
+
+prevButton.addEventListener("click", prevSlide);
+nextButton.addEventListener("click", nextSlide);
+
+
+/*nav au clavier*/
+document.addEventListener("keydown", keyboardListener);
+
+function keyboardListener(event){
+    if(event.code == "ArrowLeft"){
+        prevSlide();
+    }else if(event.code == "ArrowRight"){
+        nextSlide();
+    }
+}
+
+
+
+function prevSlide(){
+     let activeSlideEl = document.querySelector(".slider__el--show");
+     let prevSlideEl = activeSlideEl.previousElementSibling;
+     if(!prevSlideEl){
+        prevSlideEl = activeSlideEl.parentNode.lastElementChild;
+     }
+    activeSlideEl.classList.remove("slider__el--show");
+    prevSlideEl.classList.add("slider__el--show");
+}
+
+function nextSlide(){
+    let activeSlideEl = document.querySelector(".slider__el--show");
+    let nextSlideEl = activeSlideEl.nextElementSibling;
+    if(!nextSlideEl){
+       nextSlideEl = activeSlideEl.parentNode.firstElementChild;
+    }
+   activeSlideEl.classList.remove("slider__el--show");
+   nextSlideEl.classList.add("slider__el--show");
+}
